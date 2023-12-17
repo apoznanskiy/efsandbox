12ae3e34a91c9e3f159641f98a465abc75cf0df4
Andrii Poznanskyi <andrii.poznanskyi@lynceetec.com>
2023-12-17 20:02:14 +0200

    EF module 4 changes

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,9 +7,22 @@ public class SamuraiContext : DbContext
 {
     public DbSet<Samurai> Samurais { get; set; }
     public DbSet<Quote> Quotes { get; set; }
+    public DbSet<Battle> Battles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=DESKTOP-Q44S7N8;Database=applicationdb1;User Id=sa;Password=admin;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Samurai>()
+            .HasMany(s => s.Battles)
+            .WithMany(b => b.Samurais)
+            .UsingEntity<BattleSamurai>
+            (bs => bs.HasOne<Battle>().WithMany(),
+             bs => bs.HasOne<Samurai>().WithMany())
+            .Property(bs => bs.DateJoined)
+            .HasDefaultValueSql("getdate()");
     }
 }
---
+++ SamuraiApp.Domain/Battle.cs
@@ -0,0 +1,9 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Samurai> Samurais { get; set; } = new List<Samurai>();
+    }
+}
